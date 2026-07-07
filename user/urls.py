7e0607b44747e40c347bcbc62abd81b0b642434d
JulianskyMe <juliakamynskaya@gmail.com>
2026-07-07 18:53:30 +0300

    Solution

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-from user.views import CreateUserView, LoginUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="get_token")
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage")
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from user.serializers import UserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -11,3 +13,12 @@ class CreateUserView(generics.CreateAPIView):
 
 class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
